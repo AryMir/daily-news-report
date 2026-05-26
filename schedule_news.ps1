67723e3f6418656563ef97b557f88cb89323adf7
AryMir <49686569+AryMir@users.noreply.github.com>
2026-05-26 11:19:27 -0700

    Delete schedule_news.ps1

--- schedule_news.ps1
+++
@@ -1,3 +0,0 @@
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"k:\Google Drive\KNOWLEDGEBASE\Antigravity\master_news_automation.ps1`""
-$Trigger = New-ScheduledTaskTrigger -Daily -At "7:00AM"
-Register-ScheduledTask -TaskName "Antigravity Daily News" -Action $Action -Trigger $Trigger -Description "Generates and emails the Daily News Report via Gemini API" -Force
